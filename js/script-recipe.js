fbad7c5089c4a39b9c5c39404f1c38133e819a24
carola <carolacancinoc@gmail.com>
2016-12-19 22:50:47 -0300

    etapa 7 imprimir HTML

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,5 +1,8 @@
-//sacar menu
-$(document).ready(function(){ 
  
-    $('js-menu').hide(
-    	});
+$(document).ready(function(){ 
+ //sacar menu
+    $('js-menu').hide();
+//docmento html listo imprimir
+ 	console.log('HTML Recipe Listo');
+
+ });
